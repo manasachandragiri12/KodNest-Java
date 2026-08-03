81f4c717e3249bc5850e46ab40e64bc0dbfbf055
MANASA <manasachandragiri12@gmail.com>
2026-08-03 12:49:34 +0530

    Added LearnerProfile program

---
+++ track/M02-T01-St01-P01/LearnerProfile.java
@@ -0,0 +1,8 @@
+public class LearnerProfile {
+    public static void main(String[] args) {
+        String LearnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + LearnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
