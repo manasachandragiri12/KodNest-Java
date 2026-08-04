e7ed250d84bcbc6eb38961873b1b30b2d7eb7c06
MANASA <manasachandragiri12@gmail.com>
2026-08-04 11:52:47 +0530

    Added MarksPercentage program

---
+++ track/M02-T01-St01-P01/MarksPercentage.java
@@ -0,0 +1,25 @@
+public class MarksPercentage {
+    public static void main(String[] args) {
+
+        double principal = 10000.0;
+        double rate = 6.5;
+        double time = 2.0;
+        double weight = 72.0;
+        double height = 1.8;
+
+        int totalMarks = 78 + 84 + 69 + 91 + 88;
+
+        double simpleInterest = principal * rate * time / 100.0;
+        double totalAmount = principal + simpleInterest;
+
+        double bmi = weight / (height * height);
+
+        double percentage = totalMarks * 100.0 / 500;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage);
+    }
+}
