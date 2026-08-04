f40eb5fe90fcbfc4e4bcff02b67d15ae7b3e818c
MANASA <manasachandragiri12@gmail.com>
2026-08-04 12:27:56 +0530

    Added Positive program

---
+++ track/Practice Problems/M01/T01/P01/Positive.java
@@ -0,0 +1,12 @@
+public class Positive {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
