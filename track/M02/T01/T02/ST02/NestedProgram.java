3ad50e03cb999afd966c5612e54e4445f888575e
MANASA <manasachandragiri12@gmail.com>
2026-08-06 12:48:35 +0530

    Added NestedProgram program

--- track/M02/T01/T02/ST02/NestedProgram.java
+++ track/M02/T01/T02/ST02/NestedProgram.java
@@ -1,7 +1,7 @@
 public class NestedProgram {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 4; j++) {
                 if (i == 2) {
                     continue;
                 }
