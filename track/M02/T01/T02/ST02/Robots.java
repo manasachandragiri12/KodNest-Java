d51c69db32f140e970e2165a7fa059cdf1c34b80
MANASA <manasachandragiri12@gmail.com>
2026-08-11 12:13:28 +0530

    Robots

---
+++ track/M02/T01/T02/ST02/Robots.java
@@ -0,0 +1,12 @@
+class Robot {
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+public class Robots {
+    public static void main(String[] args) {
+        Robot r = new Robot();
+        r.speak();
+    }
+}
