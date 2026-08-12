57cf75de878acb16918d053000635d6075352bfd
MANASA <manasachandragiri12@gmail.com>
2026-08-12 12:33:29 +0530

    shadowing

---
+++ track/M02/T01/T02/ST02/shadowing.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class ShadowEmployee {
+    String name;
+    double salary;
+
+    void setDetails(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class shadowing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        double salary = scanner.nextDouble();
+
+        ShadowEmployee e1 = new ShadowEmployee();
+
+        e1.setDetails(name, salary);
+        e1.displayDetails();
+
+        scanner.close();
+    }
+}
