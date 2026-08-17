8965794efc7f8b2b9c83a955c32333c29ddcf741
MANASA <manasachandragiri12@gmail.com>
2026-08-17 12:49:31 +0530

    Prgramarray

---
+++ track/M02/T01/T02/ST02/lockacess.java
@@ -0,0 +1,48 @@
+class unlock {
+    void unlock(int number) {
+        System.out.println("Unlock using number");
+    }
+
+    void unlock(pattern P) {
+        System.out.println("Unlock using pattern");
+    }
+
+    void unlock(face f) {
+        System.out.println("Unlock using face");
+    }
+
+    void unlock(finger f) {
+        System.out.println("Unlock using finger");
+    }
+
+    void unlock(alphanum a) {
+        System.out.println("Unlock using alphanum");
+    }
+
+    class fingerprint {
+
+    }
+
+    class facerecognition {
+
+    }
+
+    class pattern {
+
+    }
+
+    class pin {
+
+    }
+
+    class Alphanumeric {
+
+    }
+}
+
+class lockaccess {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ track/M02/T01/T02/ST02/programarrays.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class programarrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter Array Elements:");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
