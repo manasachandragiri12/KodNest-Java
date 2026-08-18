384836db78cd08cfbfebb116da58823c0c6acf1a
MANASA <manasachandragiri12@gmail.com>
2026-08-18 18:24:37 +0530

    program

---
+++ track/M02/T01/T02/ST02/reorder.java
@@ -0,0 +1,15 @@
+public class reorder {
+    public static void main(String[] args) {
+        int[] stock = { 4, 12, 2, 9, 5 };
+        int reorderLevel = 5;
+        int reorderCount = 0;
+
+        for (int i = 0; i < stock.length; i++) {
+            if (stock[i] <= reorderLevel) {
+                reorderCount++;
+            }
+        }
+
+        System.out.println("Items to reorder: " + reorderCount);
+    }
+}
