0568983590587baa5cc76ec773a919ecb9b6c26f
MANASA <manasachandragiri12@gmail.com>
2026-09-07 17:44:37 +0530

    readText

---
+++ track/M02/T01/T02/ST02/M04/readText.java
@@ -0,0 +1,17 @@
+package M04;
+
+import java.util.Scanner;
+
+public class readText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String learnerName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        // Display the name, city and name length.
+        System.out.println("Name: " + learnerName);
+        System.out.println("City: " + city);
+        System.out.println("Name length: " + learnerName.length());
+    }
+}
